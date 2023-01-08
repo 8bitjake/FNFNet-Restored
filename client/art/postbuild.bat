dbdeaecb8a56536769197ef41e7cc98a67980e98
8Bit <75950907+8bitjake@users.noreply.github.com>
2023-01-08 16:35:19 -0600

    Massive L

--- client/art/postbuild.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo =================================
-echo .
-echo Thanks for compiling TrollEngine!
-echo .
-echo PS: if you want to remove features check out the docs :)
-echo .
-echo =================================
